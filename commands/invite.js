0f5e7e7248b6b74e3aff85c5fa9708caf2198e3c
DevuGamerOP <121098654+DevuGamerOP@users.noreply.github.com>
2024-02-02 12:09:46 +0530

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,32 @@
+const { ApplicationCommandOptionType } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');  EmbedBuilder
+const { owner } = require('../config.json'); 
+
+module.exports = {
+  name: "invite",
+  description: "Get an invite link for the bot.",
+  permissions: "0x0000000000000800",
+  options: [],
+
+  run: async (client, interaction) => {
+    try {
+      const InviteLink = 'YOUR_BOT_INVITE_LINK'; 
+      const SupportLink = 'YOUR_SUPPORT_SERVER_LINK'; 
+
+      const embed = new EmbedBuilder()
+        .setColor('#da2a41')
+        .setAuthor({
+          name: 'Invite Bot',
+          iconURL: 'YOUR_BOT_ICON_URL', 
+          url: InviteLink
+        })
+        .setDescription(Invite me to your server using [this link](${InviteLink}).\nJoin our support server: [Support Server](${SupportLink}).)
+        .setTimestamp();
+
+      interaction.reply({ embeds: [embed] }).catch(e => console.error(e));
+
+    } catch (e) {
+      console.error(e);
+    }
+  },
+};
