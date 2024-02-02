fb6ed08a6b8a4803fc096c9f8484b84561fa9319
DevuGamerOP <121098654+DevuGamerOP@users.noreply.github.com>
2024-02-02 12:03:19 +0530

    Delete commands/membercountchart.js

--- commands/membercountchart.js
+++
@@ -1,7 +0,0 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const QuickChart - require('quickchart-js'); //Run npm i quickchart-js in terminal
-
-module.exports = {
-    data: new SlashCommandBuilder()
-    .setName('membercount-chart')
-}
